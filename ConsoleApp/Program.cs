f461bf71c8ff38b03998419cb9fb68505ffb1d23
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-06-03 20:28:06 +1000

    DB Seeding

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,7 +1,9 @@
-﻿using EntityFramework;
-using NinjaDomain.DataModel;
+﻿using NinjaDomain.DataModel;
 using System;
 using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+using EntityFramework;
 
 namespace ConsoleApp
 {
@@ -10,8 +12,16 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             Database.SetInitializer(new NullDatabaseInitializer<NinjaContext>());
-            InsertNinja();
+            // InsertNinja();
+            // InsertMultipleNinjas();
+            //SimpleNinjaQueries();
+            //QueryAndUpdateNinja();
+
+            Console.ReadKey();
         }
+
+
+
         private static void InsertNinja()
         {
             var ninja = new Ninja
@@ -22,14 +32,68 @@ namespace ConsoleApp
                 ClanId = 1
 
             };
-            using (var context = new NinjaContext())
+            using (NinjaContext context = new NinjaContext())
             {
                 context.Database.Log = Console.WriteLine;
                 context.Ninjas.Add(ninja);
                 context.SaveChanges();
             }
+        }
 
 
+        private static void InsertMultipleNinjas()
+        {
+            var ninja1 = new Ninja
+            {
+                Name = "Leonardo",
+                ServedInOniwaban = false,
+                DateOfBirth = new DateTime(1984, 1, 1),
+                ClanId = 1
+            };
+            var ninja2 = new Ninja
+            {
+                Name = "Raphael",
+                ServedInOniwaban = false,
+                DateOfBirth = new DateTime(1985, 1, 1),
+                ClanId = 1
+            };
+            using (var context = new NinjaContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                context.Ninjas.AddRange(new List<Ninja> { ninja1, ninja2 });
+                context.SaveChanges();
+            }
+        }
+
+        private static void SimpleNinjaQueries()
+        {
+            using (var context = new NinjaContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                var ninjas = context.Ninjas
+                    .Where(n => n.DateOfBirth >= new DateTime(1984, 1, 1))
+                    .OrderBy(n => n.Name)
+                    .Skip(1).Take(1);
+
+                //var query = context.Ninjas;
+                // var someninjas = query.ToList();
+                foreach (var ninja in ninjas)
+                {
+                    Console.WriteLine(ninja.Name);
+                }
+            }
+        }
+
+        private static void QueryAndUpdateNinja()
+        {
+            using (var context = new NinjaContext())
+            {
+                context.Database.Log = Console.WriteLine;
+                var ninja = context.Ninjas.FirstOrDefault();
+                ninja.ServedInOniwaban = (!ninja.ServedInOniwaban);
+                context.SaveChanges();
+            }
         }
     }
+     
 }
